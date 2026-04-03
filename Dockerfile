46c47b9dcb1a878dd7dd69860509cc9356617447
Kananelo <kani4workonly@gmail.com>
2026-04-03 19:26:34 +0200

    Added register page and updated project for deployment

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,5 @@
 # Step 1: Use the official .NET 8 SDK to build your app
-FROM mcr.microsoft.com/dotnet/sdk:8.0 AS build
+FROM mcr.microsoft.com/dotnet/sdk:10.0 AS build
 WORKDIR /src
 
 # Copy everything and build the release version
@@ -7,7 +7,7 @@ COPY . ./
 RUN dotnet publish -c Release -o /app/publish
 
 # Step 2: Use the lighter .NET runtime to actually run the app
-FROM mcr.microsoft.com/dotnet/aspnet:8.0 AS runtime
+FROM mcr.microsoft.com/dotnet/aspnet:10.0 AS runtime
 WORKDIR /app
 COPY --from=build /app/publish .
 
--- GameCenterApi/obj/Debug/net10.0/GameCenterApi.AssemblyInfo.cs
+++ GameCenterApi/obj/Debug/net10.0/GameCenterApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GameCenterApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bc864beb22285ccdb80acd0306e8c5e7802b080d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+075b5b115a8e93e224766fd5841467624fd534a3")]
 [assembly: System.Reflection.AssemblyProductAttribute("GameCenterApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GameCenterApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
