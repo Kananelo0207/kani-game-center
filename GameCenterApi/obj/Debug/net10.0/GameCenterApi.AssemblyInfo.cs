4400e05d0703ad125ccf7f7a1c744ef1903a75cd
Kananelo <kani4workonly@gmail.com>
2026-04-05 10:58:04 +0200

    done

--- GameCenterApi/obj/Debug/net10.0/GameCenterApi.AssemblyInfo.cs
+++ GameCenterApi/obj/Debug/net10.0/GameCenterApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GameCenterApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3518e180a1c2b2ad47afc3d72ba518327fd47c8d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0c4a87df83ad145b71cb4cd6e772f9531813a93a")]
 [assembly: System.Reflection.AssemblyProductAttribute("GameCenterApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GameCenterApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
