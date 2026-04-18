ef17906ce1f7362eabce9802d1b6005525cada37
Kananelo <kani4workonly@gmail.com>
2026-04-18 17:28:50 +0200

    Add 300 words to Hangman word bank and update file structure

--- GameCenterApi/obj/Debug/net10.0/GameCenterApi.AssemblyInfo.cs
+++ GameCenterApi/obj/Debug/net10.0/GameCenterApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GameCenterApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b15d9680f59dd0e415a9d9137d4cb47d7c89b35a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+708ae96e55e53a1b37c1923653a3c0bd4d513d7b")]
 [assembly: System.Reflection.AssemblyProductAttribute("GameCenterApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GameCenterApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
